66e3907b293cb012d96f3d2d9423d29ab0085773
Giovanni Nespoli <ggnespoli@gmail.com>
2025-02-25 16:30:22 -0300

    chore: commit verify mensage

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+// commitlint.config.js
+
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  // If you need custom rules, add them below:
+  // rules: {
+  //   'type-enum': [
+  //     2,
+  //     'always',
+  //     [
+  //       'build',
+  //       'chore',
+  //       'ci',
+  //       'docs',
+  //       'feat',
+  //       'fix',
+  //       'perf',
+  //       'refactor',
+  //       'revert',
+  //       'style',
+  //       'test',
+  //     ],
+  //   ],
+  // },
+};
